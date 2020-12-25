399c5adc70e821c8f09dce5fa7a1f6128ba42113
Tomoyuki Sakurai <y@trombik.org>
2020-12-25 12:13:56 +0700

    bugfix: use shebangfix, remove unnecessary patch

--- cad/cura/files/patch-cura__app.py
+++
@@ -1,8 +0,0 @@
---- cura_app.py.orig	2020-11-10 10:30:06 UTC
-+++ cura_app.py
-@@ -1,4 +1,4 @@
--#!/usr/bin/env python3
-+#!/usr/bin/env /usr/local/bin/python3.7
- 
- # Copyright (c) 2020 Ultimaker B.V.
- # Cura is released under the terms of the LGPLv3 or higher.
